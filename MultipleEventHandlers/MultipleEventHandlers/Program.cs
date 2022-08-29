c9ffb55cd383094c57436eab1780e913ca778406
sajjadarashhh <72276897+sajjadarashhh@users.noreply.github.com>
2022-08-30 01:33:30 +0430

    new

--- MultipleEventHandlers/MultipleEventHandlers/Program.cs
+++ MultipleEventHandlers/MultipleEventHandlers/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using MultipleEventHandlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var jsRuntime = builder.Services.BuildServiceProvider().GetRequiredService<IJSRuntime>();
+Lazy<Task<IJSObjectReference>> _moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+   "import", "./file.js").AsTask());
+var module = await _moduleTask.Value;
+await module.InvokeVoidAsync("mountAndInitializeDb");
 
 await builder.Build().RunAsync();
---
+++ MultipleEventHandlers/MultipleEventHandlers/wwwroot/file.js
@@ -0,0 +1,21 @@
+﻿export function mountAndInitializeDb() {
+    window.Module.FS.mkdir('/database');
+    //#IDBFS Not Defined
+    window.Module.FS.mount(IDBFS, {}, '/database');
+    return syncDatabase(true);
+}
+
+export function syncDatabase(populate) {
+    return new Promise((resolve, reject) => {
+        window.Module.FS.syncfs(populate, (err) => {
+            if (err) {
+                console.log('syncfs failed. Error:', err);
+                reject(err);
+            }
+            else {
+                console.log('synced successfull.');
+                resolve();
+            }
+        });
+    });
+}
